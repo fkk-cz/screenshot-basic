831c2f4a7ed9cf713118871d80be1b22d2a3b04a
Taso <taso@projecterror.dev>
2021-07-27 22:15:17 -0400

    tweak(build): setup for standalone build without embedded yarn/webpack
    Since FXServers's embedded webpack and yarn like to act up, lets build it ourselves

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,13 +5,6 @@ game 'gta5'
 client_script 'dist/client.js'
 server_script 'dist/server.js'
 
-dependency 'yarn'
-dependency 'webpack'
-
-webpack_config 'client.config.js'
-webpack_config 'server.config.js'
-webpack_config 'ui.config.js'
-
 files {
     'dist/ui.html'
 }
